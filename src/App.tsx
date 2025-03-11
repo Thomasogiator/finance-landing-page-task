368b1533736aff8c4f472f71316a0d76b93d6f97
Thomas Ogiator <thomasogiator@gmail.com>
2025-03-11 14:10:47 +0100

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,37 @@
+import { useState } from 'react';
 import backgroundImg from './assets/backgroundImg.svg'
 import backgroundImg2 from './assets/backgroundImg2.svg'
 import MainComponent from './components/MainComponent';
+import ReactSwitch from 'react-switch';
 
 function App() {
+  const [darkMode, setDarkMode] = useState(false)
+
+  const handleModeChange = (nextChecked: boolean) => {
+    setDarkMode(nextChecked);
+  };
+
   return (
-    <div className="page-background">
+    <div className={darkMode ? "page-background dark-page" : "page-background"}>
+      <div className='switch'>
+        <ReactSwitch
+          checked={darkMode}
+          onChange={handleModeChange}
+          activeBoxShadow='none'
+          width={52}
+          height={32}
+          checkedIcon={false}
+          uncheckedIcon={false}
+        />
+        {darkMode ? 'Switch to Light Mode' : 'Switch to Dark Mode'}
+      </div>
       <div className='background-img'>
         <img src={backgroundImg} alt="background-image" />
       </div>
       <div className='background-img2'>
         <img src={backgroundImg2} alt="background-image" />
       </div>
-      <MainComponent/>
+      <MainComponent darkMode={darkMode}/>
     </div>
   );
 }
--- src/components/MainComponent.tsx
+++ src/components/MainComponent.tsx
@@ -1,6 +1,9 @@
 import twitter from '../assets/twitter.svg'
 import facebook from '../assets/facebook.svg'
 import medium from '../assets/medium.svg'
+import twitter2 from '../assets/twitter (1).svg'
+import facebook2 from '../assets/facebook (1).svg'
+import medium2 from '../assets/medium (1).svg'
 import financeLogo from '../assets/financeLogo.svg'
 import align from '../assets/align-text-left.svg'
 import revenue from '../assets/Revenue-bro.svg'
@@ -8,16 +11,16 @@ import leftBackground from '../assets/Group 3510.svg'
 import scrollDown from '../assets/Vector.svg'
 import rightback from '../assets/Group 3444.svg'
 
-const MainComponent = () => {
+const MainComponent = ({darkMode}: {darkMode: boolean}) => {
   return (
     <div className='main-comp-container'>
         <header>
             <div className='header-comp'>
-                <img src={twitter} alt="twitter" />
-                <img src={facebook} alt="facebook" />
-                <img src={medium} alt="medium" />
+                <img src={darkMode ? twitter2 : twitter} alt="twitter" />
+                <img src={darkMode ? facebook2 : facebook} alt="facebook" />
+                <img src={darkMode ? medium2 : medium} alt="medium" />
             </div>
-            <div className='header-comp'>
+            <div className={darkMode ? 'header-comp dark-mode-finance' : 'header-comp'}>
                 <img src={financeLogo} alt="finance logo" />
                 Finance
             </div>
@@ -28,7 +31,7 @@ const MainComponent = () => {
         <main>
             <div className='mutual-investment'>
                 <div className='about-investment'>
-                    <h2>Mutual Investment</h2>
+                    <h2 className={darkMode ? 'dark-mode-h2' : ''}>Mutual Investment</h2>
                     <p>At Finance we care about your future. We help you invest the way you want. So you can relax, have fun and let your fund grow.</p>
                 </div>
                 <div className='learn-more'>
